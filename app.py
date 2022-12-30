b885c7ddbd6d1c07ba40f4e9177e40e71a576dea
Laban Waititu <62809913+LW-DEVOPS@users.noreply.github.com>
2022-12-30 17:27:33 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+print('Laban Waititu')
+print('0----')
+print(' ||||')
+print('*' * 10)
+
+price = 10
+price = 20
+print(price)
+
+print('John Smith')
+print('20years')
+is_patient_new_or_existing = True
+
+name = input('What is your name? ')
+print('Hi ' + name)
+
+name = input('What is your name? ')
+color = input('What is your favourite color ')
+print(name + ' likes ' + color)
+
+birth_year = input('Birth year: ')
+age = 2022 -int(birth_year)
+print(age)
+
+weight = input('Weight in pounds: ')
+kg = float(0.453) * int(weight)
+print(kg)
+
